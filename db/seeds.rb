d51266dc4be43c651b7c142cd84ee3b611139b6b
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-11-07 22:11:52 -0800

    Load sports and leagues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+%w(baseball basketball football hockey soccer).each do |name|
+  Sport.find_or_create(name: name)
+end
+
+sports = Sport.all.reduce({}) { |h, s| h[s.name] = s.id; h }
+[
+  { abbreviation: 'MLB', name: 'Major League Baseball', sport: 'baseball' },
+  { abbreviation: 'MLS', name: 'Major League Soccer', sport: 'soccer' },
+  { abbreviation: 'NBA', name: 'National Basketball Association', sport: 'basketball' },
+  { abbreviation: 'NFL', name: 'National Football League', sport: 'football' },
+  { abbreviation: 'NHL', name: 'National Hockey League', sport: 'hockey' },
+].each do |league|
+  League.find_or_create(
+    abbreviation: league[:abbreviation],
+    name: league[:name],
+    sport_id: sports[league[:sport]],
+  )
+end
