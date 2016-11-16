a8bcc9fc91a6d98cf3672c8ff1562bc8585e8451
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-11-16 00:31:35 -0800

    Add fixture support

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,17 @@
 ENV['RAILS_ENV'] ||= 'test'
+require 'fixture_dependencies/sequel'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+#load "#{Rails.root}/db/seeds.rb"
+
+FixtureDependencies.fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
 
 class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
+  def fixture(att)
+    FixtureDependencies.load(att)
+  end
+
+  def run(*args, &block)
+    Sequel::Model.db.transaction(rollback: :always, auto_savepoint: true) { super }
+  end
 end
